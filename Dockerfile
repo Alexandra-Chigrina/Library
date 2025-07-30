eec9ecf3486ea1cfc93e9c1c96d14c2ff97dfbe9
Sasha <a.chigrina89@gmail.com>
2025-07-30 16:00:43 +0300

    init commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.13-slim
+
+WORKDIR /app
+
+RUN apt-get update \
+    && apt-get install -y gcc libpg-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY pyproject.toml poetry.lock ./
+
+RUN pip install --no-cache-dir poetry
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-root --no-interaction
+
+COPY . .
+
+EXPOSE 8000
---
+++ users/management/commands/createadmin.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User = get_user_model()
+        user = User.objects.create(
+            email='testadmin@sky.pro',
+            username='TestAdmin',
+            first_name='Admin1',
+            last_name='Admin2',
+        )
+
+        user.set_password('1234')
+
+        user.is_staff= True
+        user.is_superuser = True
+
+        user.save()
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully created admin user with email {user.email}!'))
